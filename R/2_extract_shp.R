5660e421f22518205f075a9d7e6e1a83ed7f8647
FirmianoKR <kelerocha@gmail.com>
2020-04-02 11:56:43 -0300

    separação de scripts por tarefas executadas: clip e union. ainda não deu certo, pq o R arrega na hora de salvar os clips :-p

---
+++ R/2_extract_shp.R
@@ -0,0 +1,68 @@
+#########################################
+# SPATIAL EXTRATION IN R 
+# Danielle de Oliveira Moreira
+# Date: 31/03/2020                  
+# Modified by Kele R. Firmiano in 01/04/20
+########################################
+
+# Loading packages ####
+library(rgdal)
+library(raster)
+library(maptools)
+library(rgeos) # clip
+
+# Loading shp file ####
+mg <- readOGR(dsn = "./outputs/reproj_shp", layer = "mg_mine_wgs84")
+es <- readOGR(dsn = "./outputs/reproj_shp", layer = "es_mine_wgs84")
+bhrd <- readOGR(dsn = "./outputs/reproj_shp", layer = "bhrd_lim_wgs84")
+munic <- readOGR(dsn = "./outputs/reproj_shp", layer = "munic_wgs84")
+
+# Checking coordinate system ####
+crs(mg) 
+crs(es) 
+crs(bhrd) 
+crs(munic) 
+
+# simple plots
+par(mfrow = c(2, 2), mar = c(5, 5, 4, 1))
+plot(mg , axes = TRUE, xlab = "mg_mine_wgs84")
+plot(es, axes = TRUE, xlab = "es_mine_wgs84")
+plot(bhrd, axes = TRUE, xlab = "bhrd_lim_wgs84")
+plot(munic, axes = TRUE, xlab = "munic_wgs84")
+dev.off()
+
+plot(mg, col = 'blue')
+plot(es, add = TRUE, col = 'red', axes = TRUE)
+
+# Step 1: Clipping polygons
+# clipping 
+clip_mg_bhrd_lim <- gIntersection(mg, bhrd, byid = TRUE, drop_lower_td = TRUE)
+clip_es_bhrd_lim <- gIntersection(es, bhrd, byid = TRUE, drop_lower_td = TRUE)
+clip_mg_bhrd_munic <- gIntersection(mg, munic, byid = TRUE, drop_lower_td = TRUE)
+clip_es_bhrd_munic <- gIntersection(es, munic, byid = TRUE, drop_lower_td = TRUE)
+
+# check clip 
+plot(clip_mg_bhrd_lim, col = 'blue')
+plot(clip_es_bhrd_lim, add = TRUE, col = 'red', axes = TRUE)
+plot(clip_mg_bhrd_munic, col = 'green')
+plot(clip_es_bhrd_munic, add = TRUE, col = 'orange', axes = TRUE)
+
+# save new shp ####
+writeOGR(clip_mg_bhrd_lim, "./ouputs", "clip", driver = "ESRI Shapefile", overwrite_layer = TRUE)
+writeOGR(clip_es_bhrd_lim, "./ouputs", "clip", driver = "ESRI Shapefile", overwrite_layer = TRUE)
+writeOGR(clip_mg_bhrd_munic, "./ouputs", "clip", driver = "ESRI Shapefile", overwrite_layer = TRUE)
+writeOGR(clip_es_bhrd_munic, "./ouputs", "clip", driver = "ESRI Shapefile", overwrite_layer = TRUE)
+
+# Remove unecessary files ####
+# good to get more space if you'll continue the analysis in the sequence
+rm(mg)
+rm(es)
+rm(bhrd)
+rm(munic)
+
+
+
+
+
+
+
---
+++ R/3_union_shp.R
@@ -0,0 +1,53 @@
+#########################################
+# SPATIAL UNION IN R 
+# Danielle de Oliveira Moreira
+# Date: 31/03/2020                  
+# Modified by Kele R. Firmiano in 01/04/20
+########################################
+
+# Loading packages ####
+library(rgdal)
+library(raster)
+library(maptools)
+library(rgeos)
+
+# Loading shp file ####
+mg_bhrd_lim <- readOGR(dsn = "./ouputs/clip", layer = "clip_mg_bhrd_lim")
+es_bhrd_lim <- readOGR(dsn = "./ouputs/clip", layer = "clip_es_bhrd_lim")
+mg_bhrd_munic <- readOGR(dsn = "./ouputs/clip", layer = "clip_mg_bhrd_munic")
+es_bhrd_munic <- readOGR(dsn = "./ouputs/clip", layer = "clip_es_bhrd_munic")
+
+# Checking coordinate system ####
+crs(mg_bhrd_lim) 
+crs(es_bhrd_lim) 
+crs(mg_bhrd_munic) 
+crs(es_bhrd_munic) 
+
+# simple plots
+par(mfrow = c(2, 2), mar = c(5, 5, 4, 1))
+plot(mg_bhrd_lim , axes = TRUE, xlab = "clip_mg_bhrd_lim")
+plot(es_bhrd_lim, axes = TRUE, xlab = "clip_es_bhrd_lim")
+plot(mg_bhrd_munic, axes = TRUE, xlab = "clip_mg_bhrd_munic")
+plot(es_bhrd_munic, axes = TRUE, xlab = "clip_es_bhrd_munic")
+dev.off()
+  
+# Union shp #### 
+union_bhrd_mine_lim <- union(mg_bhrd_lim, es_bhrd_lim)
+union_bhrd_mine_munic <- union(mg_bhrd_munic, es_bhrd_munic)
+
+# check union
+plot(union_bhrd_mine_lim, col = 'brown', xlab = "union_bhrd_mine_lim", axes = TRUE)
+
+plot(union_bhrd_mine_munic, col = 'brown', xlab = "union_bhrd_mine_minic", axes = TRUE)
+
+# Saving the new shp ####
+writeOGR(union_bhrd_mine_lim, "./ouputs", "union_shp", driver = "ESRI Shapefile", overwrite_layer = TRUE)
+
+writeOGR(union_bhrd_mine_munic, "./ouputs", "union_shp", driver = "ESRI Shapefile", overwrite_layer = TRUE)
+
+# Remove unecessary files ####
+# good to get more space if you'll continue the analysis in the sequence
+rm(mg_bhrd_lim)
+rm(es_bhrd_lim) 
+rm(mg_bhrd_munic) 
+rm(es_bhrd_munic) 
