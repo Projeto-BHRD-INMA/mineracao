3a4f9aaa05fa06c605cc08a7fccf57bad768bbd8
FirmianoKR <kelerocha@gmail.com>
2020-04-20 15:15:20 -0300

    to combine mine and cities information in a same shp.

---
+++ R/2_2_extract_mine-within_munic.R
@@ -0,0 +1,41 @@
+#########################################
+# SPATIAL EXTRATION IN R 
+# extracting mine information to each municipality
+# Kele R. Firmiano 
+# Date: 20/04/2020                  
+########################################
+
+# Loading packages ####
+library(rgdal)
+library(raster)
+library(rgeos) # clip
+
+# Loading shp file ####
+munic <-
+  readOGR(dsn = "./outputs/reproj_shp",
+          layer = "munic_wgs84",
+          encoding = 'UTF-8')
+mg_bhrd_munic <-
+  readOGR(dsn = "./outputs/clip_shp",
+          layer = "crop_mg_bhrd_munic",
+          encoding = 'UTF-8')
+es_bhrd_munic <-
+  readOGR(dsn = "./outputs/clip_shp",
+          layer = "crop_es_bhrd_munic",
+          encoding = 'UTF-8')
+
+# Checking coordinate system ####
+crs(munic) 
+crs(mg_bhrd_munic) 
+crs(es_bhrd_munic) 
+
+# simple plots
+plot(munic, axes = TRUE, xlab = "cities within bhrd")
+
+par(mfrow = c(1, 2), mar = c(5, 5, 4, 1))
+plot(mg_bhrd_munic, axes = TRUE, xlab = "mg_bhrd_munic")
+plot(es_bhrd_munic, axes = TRUE, xlab = "es_bhrd_munic")
+dev.off()
+
+
+
