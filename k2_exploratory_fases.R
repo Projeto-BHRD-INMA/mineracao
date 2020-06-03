1709eb9935e6d41ddbe2e5dfdf0bbb0bd6851f60
FirmianoKR <54898043+FirmianoKR@users.noreply.github.com>
2020-06-03 14:46:49 -0300

    Explorando os dados gerados pelo Bruno

---
+++ k2_exploratory_fases.R
@@ -0,0 +1,9 @@
+#############################
+# Exploratory mine fases
+# Kele R. Firmiano
+# Data: 02/06/2020
+#############################
+
+fases <- read.csv("./outputs/b2_mine_fases_UPH.csv", sep = ",", h = T)
+
+# Unidades de planejamento hídrico: Piranga, Piracicaba, Sto Antonio, Suaçui Grande, Caratinga, Manhuaçu, Guandu (507), Sta Mª do Doce (508), São José (509)
